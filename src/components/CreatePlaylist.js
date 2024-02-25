d8c5345174e635e03ca3487321462a9d1a9e025e
robertgouveia <robertgouveia03@hotmail.com>
2024-02-25 10:53:34 +0000

    Remove console.log from CreatePlaylist function
    The console.log(token) line was removed from the CreatePlaylist function to optimize the code and also for security purposes as it is unwise to leave logging of sensitive information like token in a production environment.

--- src/components/CreatePlaylist.js
+++ src/components/CreatePlaylist.js
@@ -1,5 +1,4 @@
 export default async function CreatePlaylist(user_id, token, playlistName){
-    console.log(token)
     const response = await fetch(`https://api.spotify.com/v1/users/${user_id}/playlists`, {
         method: 'POST',
         headers: {
